bfb8df2a8a6c08210dd282dc886e626c758bc334
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-02-23 04:46:08 -0600

    Move constant variables into const.go

---
+++ common/const.go
@@ -0,0 +1,24 @@
+package common
+
+// EmergencyType defines integer constants for the types of emergencies that the system expects
+type EmergencyType int
+
+const (
+	// UsersTableName defines the DynamoDB table used to store EmergenSeek users
+	UsersTableName = "EmergenSeekUsers"
+
+	// LambdaSecretsTable defines the DyamoDB table used to store environment variables
+	LambdaSecretsTable = "LambdaSecrets"
+
+	// Region defines the AWS VPC region used for development
+	Region = "us-east-2"
+
+	// SEVERE defines a priority 0 emergency
+	SEVERE EmergencyType = iota // 0
+
+	// MILD defines a priority 1 emergency
+	MILD // 1
+
+	// CHECKIN defines a priority 2 emergency (non-emergency)
+	CHECKIN //2
+)
