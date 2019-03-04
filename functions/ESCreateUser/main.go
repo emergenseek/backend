35561fcc4ce6099bcc6e48a1e6be22f09136728c
Annie Vo <annievo0127@gmail.com>
2019-03-04 14:21:51 -0600

    initial changed to CreateUSer()

--- functions/ESCreateUser/main.go
+++ functions/ESCreateUser/main.go
@@ -1,10 +1,19 @@
 package main
 
-const (
-	LinksTableName = "EmergenSeekUsers"
-	Region         = "us-east-2"
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/emergenseek/backend/common/models"
 )
 
-func main() {
+// Request defines the expected body parameters of an ESSendEmergencyVoiceCall invocation
+type CreateUser struct {
+	// The verified user making the change
+	CognitoID string `json:"cognito_id"`
+}
+
+function main() {
 
 }
+//func (c *DynamoDB) ListTables(input *ListTablesInput) (*ListTablesOutput, error)
