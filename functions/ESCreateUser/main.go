cf683c196ab3019b781cb642e0c949d92259ca1c
Annie Vo <annievo0127@gmail.com>
2019-03-05 14:35:35 -0600

    PUTITEMINPUT data added

--- functions/ESCreateUser/main.go
+++ functions/ESCreateUser/main.go
@@ -1,19 +1,50 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/emergenseek/backend/common/models"
 )
 
-// Request defines the expected body parameters of an ESSendEmergencyVoiceCall invocation
-type CreateUser struct {
-	// The verified user making the change
-	CognitoID string `json:"cognito_id"`
+//CreateUserDB to create user data, then READ
+func CreateUserDB() {
+	sess, err := session.NewSession(&aws.Config{Region: aws.String("us-east-2")})
+
+	svc := dynamodb.New(sess)
+
+	user := models.User{
+		FirstName: "Anni",
+		LastName:  "Xcmneo",
+		BloodType: "Rh+",
+		Age:       1230,
+		/*
+			PrimaryContacts:
+			SecondaryContacts:
+			LastKnownLocation:
+			PrimaryResidence:
+		*/
+		PhonePin:     12345,
+		CognitoID:    "cognido_id",
+		EmailAddress: "cognido_id@hotmail.com",
+	}
+
+	av, err := dynamodbattribute.MarshalMap(user)
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("EmergenSeekUsers"),
+	}
+	_, err = svc.PutItem(input)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
 }
 
-function main() {
-
+func main() {
+	CreateUserDB()
 }
-//func (c *DynamoDB) ListTables(input *ListTablesInput) (*ListTablesOutput, error)
