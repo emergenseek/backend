3ecb94c6f3a3bc5588dd18fee9ca58597e8e61aa
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-03-21 18:01:50 -0500

    Add CreateUser and GetUser database functions

--- common/database/db.go
+++ common/database/db.go
@@ -3,9 +3,12 @@ package database
 // Reference: https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/dynamo-example-read-table-item.html
 
 import (
+	"errors"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/emergenseek/backend/common"
 	"github.com/emergenseek/backend/common/models"
 )
 
@@ -36,7 +39,61 @@ func (d *DynamoConn) Create() error {
 	return nil
 }
 
-// GetUser will retrieve a user from Cognito
-func GetUser(uid string) *models.User {
-	return nil
+// GetUser will retrieve a user from the database
+func (d *DynamoConn) GetUser(uid string) (*models.User, error) {
+	// Create user struct to be searched for using provided uid
+	userKey := &models.User{
+		CognitoID: uid,
+	}
+	key, err := dynamodbattribute.MarshalMap(userKey)
+	if err != nil {
+		return nil, err
+	}
+	input := &dynamodb.GetItemInput{
+		Key:       key,
+		TableName: aws.String(common.UsersTableName),
+	}
+
+	// Search for user matching uid in table
+	result, err := d.Client.GetItem(input)
+	if err != nil {
+		return nil, err
+	}
+
+	// Unmarshal user into struct
+	user := &models.User{}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &user)
+	if err != nil {
+		return nil, err
+	}
+
+	// Cheap check for item not found
+	if user.FirstName == "" {
+		return nil, errors.New("user not found")
+	}
+
+	return user, nil
+}
+
+// CreateUser will create a user for the application
+func (d *DynamoConn) CreateUser(user *models.User) (*dynamodb.PutItemOutput, error) {
+	// Marshal user struct into map for DynamoDB
+	item, err := dynamodbattribute.MarshalMap(user)
+	if err != nil {
+		return nil, err
+	}
+
+	// Use marshalled map for PutItemInput
+	input := &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: aws.String(common.UsersTableName),
+	}
+
+	// Insert into database
+	output, err := d.Client.PutItem(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
 }
