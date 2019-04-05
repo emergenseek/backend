973df9f322744bbb1112ac85f0c696be10fde029
Simon Woldemichael <b30bd351371c686298d32281b337e8@gmail.com>
2019-04-05 12:46:46 -0500

    Add Tier field to contact model

--- common/models/contact.go
+++ common/models/contact.go
@@ -1,11 +1,16 @@
 package models
 
+import (
+	"github.com/emergenseek/backend/common"
+)
+
 // Contact defines the information of a User's contacts
 type Contact struct {
-	PhoneNumber  string   `json:"phone_number"`
-	Relationship string   `json:"relationship"`
-	FirstName    string   `json:"first_name"`
-	LastName     string   `json:"last_name"`
-	EmailAddress string   `json:"email_address"`
-	HomeAddress  *Address `json:"home_address"`
+	PhoneNumber  string           `json:"phone_number"`
+	Relationship string           `json:"relationship"`
+	FirstName    string           `json:"first_name"`
+	LastName     string           `json:"last_name"`
+	EmailAddress string           `json:"email_address"`
+	HomeAddress  *Address         `json:"home_address"`
+	Tier         common.AlertTier `json:"tier"`
 }
